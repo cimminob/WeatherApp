3271a18cea360631f2489be5a77233ab1fd9fb6a
brian desktop <cimminob@gmail.com>
2019-03-10 16:01:41 -0400

    connect to openweather api

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 document.getElementById('submit').addEventListener('click', getJSON);
 
-function getJSON(){
-    fetch('weather.json')
+function getJSON(event){
+    const zip=document.getElementById('zipcode').value;
+    console.log(zip);     
+
+    fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=b51166f933357bc4db15868fd0b0319f`)
     .then(response=>response.json())
-    .then(data=>{
+    .then(data=>{      
+        console.log(data);
+        console.log(data.name);
         let output='';
         const div=document.getElementById('current_weather');
-        data.forEach(loc=>{
-            output+=`<h4>City: ${loc.city}</h4><br><h5>Current Conditions:</h5>
+            output+=`<h4>City: ${data.name}</h4><br><h5>Current Conditions:</h5>
             <ul>
-                <li>Temperature: ${loc.temp}</li>
-                <li>${loc.condition}</li>
+                <li>Temperature: ${data.main.temp}</li>
+                <li>Current Conditions: ${data.weather.main} : ${data.weather.description}</li>
+                <li>Atmospheric Pressure: ${data.main.pressure}</li>
+                <li>Humidity: ${data.main.humidity}</li>
             </ul>
             `;
-        });
         div.innerHTML=output;
-    })
+        })
     .catch(err=>{console.log(err)});
 }
---
+++ weather.js
@@ -0,0 +1,5 @@
+class Weather {
+    constructor() {
+        this.key = 'b51166f933357bc4db15868fd0b0319f';
+    }
+}
