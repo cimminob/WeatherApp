60a2c7374e11eff7307f35ffae0cce26c5da13cf
brian desktop <cimminob@gmail.com>
2019-03-10 17:53:57 -0400

    add enter key test

--- app.js
+++ app.js
@@ -1,22 +1,40 @@
-document.getElementById('submit').addEventListener('click', getJSON);
+//assign submit id to variable
+const submit = document.getElementById('submit');
+
+//event listeners for submit  
+submit.addEventListener('click', getJSON);
+
+//test for enter key on zipcode
+document.getElementById('zipcode').addEventListener('keyup', pressEnter);
+
+function pressEnter(event) {
+    //test if enter key was pressed (code 13)
+
+         if(event.key == 'Enter'){
+        //trigger click event for submit
+        submit.click();
+    }
+}
+
 
 function getJSON(event){
     const zip=document.getElementById('zipcode').value;
     console.log(zip);     
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=b51166f933357bc4db15868fd0b0319f`)
+    fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&unites=imperial&appid=b51166f933357bc4db15868fd0b0319f`)
     .then(response=>response.json())
     .then(data=>{      
         console.log(data);
         console.log(data.name);
         let output='';
         const div=document.getElementById('current_weather');
-            output+=`<h4>City: ${data.name}</h4><br><h5>Current Conditions:</h5>
-            <ul>
-                <li>Temperature: ${data.main.temp}</li>
-                <li>Current Conditions: ${data.weather[description]}</li    >
-                <li>Atmospheric Pressure: ${data.main.pressure}</li>
-                <li>Humidity: ${data.main.humidity}</li>
+            output+=`<h4 class="red-text text-lighten-2">${data.name}</h4><br>
+            <h5 class="blue-text">Current Conditions:</h5>
+            <ul class="blue-text">
+                <li>Temp: <span class="red-text text-lighten-2">${data.main.temp} &deg;</span></li>
+                <li>Currently: <span class="red-text text-lighten-2">${data.weather[0].description}</span></li>
+                <li>Atmospheric Pressure: <span class="red-text text-lighten-2">${data.main.pressure}</span></li>
+                <li>Humidity: <span class="red-text text-lighten-2"> ${data.main.humidity}%</span></li>
             </ul>
             `;
         div.innerHTML=output;
